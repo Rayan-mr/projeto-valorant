1dda98bfff3965fb3bec1c71a1398c2467840d7b
Rayan de Moraes <rayan.moraes12@gmail.com>
2026-06-12 21:00:26 -0300

    correção de exibição

--- src/Pages/Agentes.jsx
+++ src/Pages/Agentes.jsx
@@ -23,7 +23,7 @@ function Agentes() {
   if (!agente) {
     return (
     <>
-    <main>
+    <main className="agenteCardMain">
       <h1>Agente não encontrado</h1>;
         {/* <button onClick={handleVoltar} className="botao-voltar">Voltar</button> */}
         <Link to="/projeto-valorant" className="botao-voltar" aria-label="Voltar para a lista de agentes">
@@ -43,10 +43,10 @@ function Agentes() {
          ← Voltar para a lista
         </Link>
       </div>
-      <main>
+      <main className="agenteCardMain">
         <h1>{agente.nome}</h1>
         <p>Função: {agente.funcao}</p>
-        <img src={imagemResolvida} alt={`Ilustração do agente ${agente.nome}`} width="300" />
+        <img src={imagemResolvida} alt={`Ilustração do agente ${agente.nome}`} width="300"/>
       </main>
     </div>
   );
