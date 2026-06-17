aae0c837e3e1c04235014ea79c853983f487bab1
Rayan de Moraes <rayan.moraes12@gmail.com>
2026-06-16 21:22:31 -0300

    adicionando tagManager

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './Pages/Home'
 import Agentes from './Pages/Agentes'
+import AnalyticsTracker from './Components/AnalyticsTracker/AnalyticsTracker'
 
 function App() {
  return(
   <BrowserRouter>
+  <AnalyticsTracker/>
     <Routes>
       <Route path='/projeto-valorant/' element={<Home/>}/>
 
--- src/Components/AgentesCards/AgentesCards.jsx
+++ src/Components/AgentesCards/AgentesCards.jsx
@@ -7,9 +7,20 @@ const textoAlternativo = nome
     ? `Ilustração do agente ${nome}, da função ${funcao}` 
     : "Ilustração de um agente do Valorant";
 
+
+
+    const registrarClick = (destino) =>{
+      console.log('Evento de clique enviado, destino:', destino)
+
+      window.dataLayer.push({
+        event: "click",
+        destino: destino,
+      })
+    }
+
   return (
     <div className="card">
-      <Link to={`/projeto-valorant/${id}`}>
+      <Link to={`/projeto-valorant/${id}`} onClick={() => resgitrarClick(nome)}>
         <img src={imagem} alt={textoAlternativo} />
       
 
